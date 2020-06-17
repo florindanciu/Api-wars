90b3487b22190b9cbc00b023fe714c2ab3ecf45e
Florin Danciu <danciu2020@gmail.com>
2020-06-17 08:29:17 +0300

    Added the structure

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-# put your code here
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
