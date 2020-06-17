883c6572e81a0021be6fd212cf3b8cc38320f73b
florindanciu <danciu2020@gmail.com>
2020-06-17 14:40:42 +0300

    Added next and previous page feature

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,34 @@
 window.onload = function () {
 
-    fetch('https://swapi.dev/api/planets')
-        .then((response) => response.json())
-        .then((data) => {
-            let planets = data.results
-            for (let i = 0; i < planets.length; i++) {
-                let result = data.results[i]
-                document.createElement()
-                console.log(result.terrain)
-            }
+    function getData (url, callback) {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                callback(data)
+            })
+    }
 
+    function displayData (data) {
+        let planets = data.results
+        let tBody = document.querySelector('#table > tbody')
+        tBody.innerHTML = ''
+        planets.forEach((planet) => {
+            let finalData = [planet.name, planet.diameter, planet.climate, planet.terrain, `${planet.surface_water}%`, `${planet.population} people`, planet.residents];
+            let tr = document.createElement('tr');
+            finalData.forEach((cell) => {
+                let td = document.createElement('td');
+                td.textContent = cell;
+                tr.appendChild(td);
+            })
+            tBody.appendChild(tr)
         })
 
-
+        document.querySelector('#next').onclick = function () {
+            getData(data.next, displayData);
+        }
+        document.getElementById('previous').onclick = function () {
+            getData(data.previous, displayData);
+        }
+    }
+    getData('http://swapi.dev/api/planets', displayData)
 };
